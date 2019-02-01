998a47d65adc95f8e2612d41961d45be6446efe7
evb0110 <7149553@gmail.com>
2019-02-02 00:15:51 +0300

    tests work

---
+++ src/js/destructure.js
@@ -0,0 +1,8 @@
+const makeAttackArray = (obj) => {
+  return obj.special.map(elem => {
+    const { id, name, description = 'Описание недоступно', icon } = elem;
+    return { id, name, description, icon }
+  });
+}
+
+export default makeAttackArray;
---
+++ src/js/destructure.test.js
@@ -0,0 +1,55 @@
+import makeAttackArray from './destructure';
+
+test('input object has attacks with and without description', () => {
+  const input = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон'
+      }, 
+      {
+        id: 7,
+        name: 'Двойной вы',
+        icon: 'http://...',
+      }, 
+      {
+        id: 4,
+        name: 'dsadf вы',
+        icon: 'http://...',
+      }, 
+    ]	
+  };
+
+  const expected = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон'
+    }, 
+    {
+      id: 7,
+      description: 'Описание недоступно',
+      name: 'Двойной вы',
+      icon: 'http://...',
+    }, 
+    {
+      id: 4,
+      description: 'Описание недоступно',
+      name: 'dsadf вы',
+      icon: 'http://...',
+    }, 
+  ];
+  
+  const output = makeAttackArray(input);
+
+  expect(output).toEqual(expected);
+});
